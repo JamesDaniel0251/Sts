46f1779a71151b62fcf62733955bd9921bc913cd
JamesDaniel0251 <comyeose@naver.com>
2016-11-21 10:30:15 +0900

    Test

--- Lee_Trade/src/main/java/com/lee/LeeTradeApplication.java
+++ Lee_Trade/src/main/java/com/lee/LeeTradeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeeTradeApplication {
-
+//Master
 	public static void main(String[] args) {
 		SpringApplication.run(LeeTradeApplication.class, args);
 	}
